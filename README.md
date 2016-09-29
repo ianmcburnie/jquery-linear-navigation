98139e1c3ea53e3ce60038ab7bed762c21869888
Ian McBurnie <imcburnie@hotmail.com>
2016-09-29 10:02:51 -0700

    Added autoReset and autoInit functionality

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # jquery-linear-navigation
 
-jQuery collection plugin that implements the input and model for one-dimensional
-keyboard navigation.
+jQuery collection plugin that implements the event handlers and model for one-dimensional keyboard navigation.
 
-This plugin is used by jquery-roving-tabindex and jquery-active-descendant.
-Docs to come...
+This plugin is used by jquery-roving-tabindex and jquery-active-descendant, and consolidates their common functionality and state. You probably don't need to call this plugin directly.
+
+Docs to come.
--- jquery.linearnavigation.js
+++ jquery.linearnavigation.js
@@ -1,10 +1,10 @@
 /**
-* @file jQuery collection plugin that implements the input and model for
-* one-dimensional keyboard navigation
+* @file jQuery collection plugin that implements the events and model for one-dimensional keyboard navigation
 * @author Ian McBurnie <ianmcburnie@hotmail.com>
-* @version 0.0.4
+* @version 0.1.0
 * @requires jquery
 * @requires jquery-common-keydown
+* @requires jquery-focus-exit
 */
 (function($, window, document, undefined) {
     var pluginName = 'jquery-linear-navigation';
@@ -14,103 +14,170 @@
     * @param {Object} itemsSelector - collection of navigable elements
     * @param {Object} [options]
     * @param {string} [options.axis] - set arrow key axis to x, y or both (default: both)
-    * @param {boolean} [options.wrap] - keyboard focus wraps from last to first & vice versa (default: true)
     * @param {string} [options.activeIndex] - specify the initial active item by index position (default: 0)
-    * @param {string} [options.clickDelegate] - specify an alternate delegate for click events (default: delegateEl)
-    * @param {string} [options.setFocus] - set focus when roving tab index changes (default: true)
+    * @param {string} [options.autoInit] - reset the model when focus is lost (default: false)
+    * @param {string} [options.autoReset] - reset the model when focus is lost (default: false)
+    * @param {boolean} [options.wrap] - keyboard focus wraps from last to first & vice versa (default: true)
     * @fires linearNavigationChange - when the current item changes
+    * @fires linearNavigationReset - when the model resets
+    * @fires linearNavigationInit - when the model inits
+    * @listens linearNavigationItemsChange - for changes to descendant items
     * @return {Object} chainable jQuery class
     */
     $.fn.linearNavigation = function linearNavigation(itemsSelector, options) {
         options = $.extend({
             activeIndex: 0,
-            clickable: true,
-            clickDelegate: this,
             axis: 'both',
+            debug: false,
+            autoReset: false,
+            autoInit: false,
             wrap: true
         }, options);
 
         return this.each(function onEachMatchedEl() {
-            var $this = $(this);
-            var $collection = $this.find(itemsSelector);
-            var numItems = $collection.length;
-
             if ($.data(this, pluginName) === undefined) {
-                var $clickDelegate = $(options.clickDelegate);
-                var currentItemIndex = options.activeIndex;
+                var $widget = $(this);
+                var $collection = $widget.find(itemsSelector);
+                var numItems = $collection.length;
+                var currentItemIndex = null;
+
+                var hasDoneInit = function() {
+                    return currentItemIndex !== null;
+                };
+
+                var needsInit = function() {
+                    return options.autoInit === false && hasDoneInit() === false;
+                };
+
+                var storeData = function() {
+                    $collection.each(function onEachMatchedEl(index) {
+                        // store index position on each collection item
+                        $.data(this, pluginName, {idx: index});
+                    });
+                };
+
+                var resetModel = function() {
+                    $($collection.get(currentItemIndex)).trigger("linearNavigationReset", {fromIndex: currentItemIndex, toIndex: null});
+                    currentItemIndex = null;
+                };
+
+                var initModel = function() {
+                    if (currentItemIndex !== options.activeIndex) {
+                        $($collection.get(options.activeIndex)).trigger("linearNavigationInit", {fromIndex: currentItemIndex, toIndex: options.activeIndex});
+                        currentItemIndex = options.activeIndex;
+                    }
+                };
+
+                var onFocusExit = function() {
+                    if (currentItemIndex !== null) {
+                        if (options.autoInit === false) {
+                            resetModel();
+                        } else {
+                            initModel();
+                        }
+                    }
+                };
 
                 var updateModel = function(goToIndex) {
-                    $this.trigger("linearNavigationChange", {fromIndex: currentItemIndex, toIndex: goToIndex});
-
-                    currentItemIndex = goToIndex;
-                };
-
-                var goToNextItem = function(e) {
-                    var isOnLastEl = (currentItemIndex === jQuery.data(e.delegateTarget, pluginName).length - 1);
-                    var goToIndex = currentItemIndex;
-
-                    if (isOnLastEl) {
-                        if (options.wrap === true) {
-                            goToIndex = 0;
-                        }
-                    } else {
-                        goToIndex = currentItemIndex + 1;
+                    if (goToIndex !== currentItemIndex) {
+                        $($collection.get(goToIndex)).trigger("linearNavigationChange", {fromIndex: currentItemIndex, toIndex: goToIndex});
+                        currentItemIndex = goToIndex;
                     }
-
-                    updateModel(goToIndex);
                 };
 
-                var goToPrevItem = function(e) {
-                    var isOnFirstEl = (currentItemIndex === 0 || currentItemIndex === -1);
-                    var goToIndex = currentItemIndex;
+                var onLinearNavigationItemsChange = function() {
+                    $collection = $widget.find(itemsSelector);
+                    $.data(this, pluginName).length = $collection.length;
+                    storeData();
+                };
 
-                    if (isOnFirstEl) {
-                        if (options.wrap === true) {
-                            goToIndex = jQuery.data(e.delegateTarget, pluginName).length - 1;
-                        }
+                var onClick = function() {
+                    updateModel($.data(this, pluginName).idx);
+                };
+
+                var onKeyNext = function(e) {
+                    if (needsInit() === true) {
+                        initModel();
                     } else {
-                        goToIndex = currentItemIndex - 1;
-                    }
+                        var isOnLastEl = (currentItemIndex === jQuery.data(e.delegateTarget, pluginName).length - 1);
+                        var goToIndex = currentItemIndex;
 
-                    updateModel(goToIndex);
+                        if (currentItemIndex === null) {
+                            goToIndex = options.activeIndex;
+                        } else if (isOnLastEl) {
+                            if (options.wrap === true) {
+                                goToIndex = 0;
+                            }
+                        } else {
+                            goToIndex = currentItemIndex + 1;
+                        }
+
+                        updateModel(goToIndex);
+                    }
                 };
 
-                // ensure item index is not out of bounds
-                if (currentItemIndex >= numItems) {
-                    currentItemIndex = 0;
-                }
+                var onKeyPrevious = function(e) {
+                    if (needsInit() === true) {
+                        initModel();
+                    } else {
+                        var isOnFirstEl = currentItemIndex === 0;
+                        var goToIndex = currentItemIndex;
+
+                        if (currentItemIndex === null) {
+                            goToIndex = options.activeIndex;
+                        } else if (isOnFirstEl) {
+                            if (options.wrap === true) {
+                                goToIndex = jQuery.data(e.delegateTarget, pluginName).length - 1;
+                            }
+                        } else {
+                            goToIndex = currentItemIndex - 1;
+                        }
+
+                        updateModel(goToIndex);
+                    }
+                };
 
                 // install commonKeyDown plugin on main delegate element
-                $this.commonKeyDown();
+                $widget.commonKeyDown();
 
                 // handle arrow keys
                 if (options.axis === 'x') {
-                    $this.on('leftArrowKeyDown', goToPrevItem);
-                    $this.on('rightArrowKeyDown', goToNextItem);
+                    $widget.on('leftArrowKeyDown', onKeyPrevious);
+                    $widget.on('rightArrowKeyDown', onKeyNext);
                 } else if (options.axis === 'y') {
-                    $this.on('upArrowKeyDown', goToPrevItem);
-                    $this.on('downArrowKeyDown', goToNextItem);
+                    $widget.on('upArrowKeyDown', onKeyPrevious);
+                    $widget.on('downArrowKeyDown', onKeyNext);
                 } else {
-                    $this.on('leftArrowKeyDown upArrowKeyDown', goToPrevItem);
-                    $this.on('rightArrowKeyDown downArrowKeyDown', goToNextItem);
+                    $widget.on('leftArrowKeyDown upArrowKeyDown', onKeyPrevious);
+                    $widget.on('rightArrowKeyDown downArrowKeyDown', onKeyNext);
                 }
 
                 // delegate item click events, event bound to each item
-                $clickDelegate.on('click', itemsSelector, function(e) {
-                    updateModel($(this).data(pluginName).idx);
-                });
+                $widget.on('click', itemsSelector, onClick);
 
-                // store data on delegate element
+                // update state when focus leaves the widget
+                if (options.autoReset === true) {
+                    $widget.focusExit();
+                    $widget.on('focusExit', onFocusExit);
+                }
+
+                $widget.on('linearNavigationItemsChange', onLinearNavigationItemsChange);
+
+                // store data on bound element
                 jQuery.data(this, pluginName, {installed: 'true', length: numItems});
-            } else {
-                // if plugin gets called again, update number of items
-                jQuery.data(this, pluginName).length = numItems;
-            }
 
-            $collection.each(function onEachMatchedEl(index) {
-                // store index position on each collection item
-                $(this).data(pluginName, {idx: index});
-            });
+                // we can set the intial active descendant if arrow key not required
+                if (options.autoInit === true) {
+                    setTimeout(function() {
+                        initModel();
+                    }, 0);
+                }
+
+                // store data on element
+                storeData();
+            } else if (options.debug === true) {
+                console.log('debug: {pluginName} is already installed on {element}'.replace('{pluginName}', pluginName).replace('{element}', this));
+            }
         });
     };
 }(jQuery, window, document));
@@ -129,4 +196,32 @@
 * @property {object} data - event data params
 * @param {string} [data.fromIndex] - old collection idx position
 * @param {string} [data.toIndex] - new collection idx position
+*/
+
+/**
+* linearNavigationInit event
+* @event linearNavigationInit
+* @type {object}
+* @property {object} event - event object
+* @property {object} data - event data params
+* @param {string} [data.fromIndex] - old collection idx position
+* @param {string} [data.toIndex] - new collection idx position
+*/
+
+/**
+* linearNavigationReset event
+* @event linearNavigationReset
+* @type {object}
+* @property {object} event - event object
+* @property {object} data - event data params
+* @param {string} [data.fromIndex] - old collection idx position
+* @param {string} [data.toIndex] - new collection idx position
+*/
+
+/**
+* linearNavigationItemsChange event
+*
+* @event linearNavigationItemsChange
+* @type {object}
+* @property {object} event - event object
 */
